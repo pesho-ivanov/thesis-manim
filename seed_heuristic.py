f1a5e30ac899543b01f063484eee99a48f671c93
Pesho Ivanov <ivanov@pesho.info>
2022-11-01 03:49:23 +0100

    fixes

--- seed_heuristic.py
+++ seed_heuristic.py
@@ -9,18 +9,19 @@ class SeedHeuristic(Scene):
         fsz = 28
 
         # colors [dark, light]
-        blue = ["#6C8EBF", "#DAE8FC"]
-        yellow = ["#D79B00", "#FFE6CC"]
-        purple = ["#9673A6", "#E1D5E7"]
-        green = ["#82B366", "#D5E8D4"]
-        colors = [blue, yellow, purple, green]
+        grey = "#777777"
+        blues = ["#6C8EBF", "#DAE8FC"]
+        yellows = ["#D79B00", "#FFE6CC"]
+        purples = ["#9673A6", "#E1D5E7"]
+        greens = ["#82B366", "#D5E8D4"]
+        colors = [blues, yellows, purples, greens]
         delta = [0.08*d for d in [UP+LEFT, UP+RIGHT, DOWN+LEFT, DOWN+RIGHT]]
 
         # introduce query
         k = 2
-        query = Text("AACCGGTT")
+        query = Text("AACCGGTT" )
         query.shift(1.0*(UP+RIGHT))
-        query_label = Text("query", slant=ITALIC, font_size=fsz)
+        query_label = Text("query", slant=ITALIC, font_size=fsz, color=grey)
         query_label.next_to(query, 15 * LEFT)
         self.play(Write(query_label))
         self.play(Write(query))
@@ -28,7 +29,7 @@ class SeedHeuristic(Scene):
         # introduce reference
         ref = Text("..GGAAGTCAACCGATT..") #\n\n..GATCCGGCTT..")
         ref.next_to(query, DOWN, buff=1.0)
-        ref_label = Text("reference", slant=ITALIC, font_size=fsz)
+        ref_label = Text("reference", slant=ITALIC, font_size=fsz, color=grey)
         ref_label.next_to(ref, LEFT)
         ref_label.align_to(query_label, LEFT)
         self.play(Write(ref_label))
@@ -42,15 +43,18 @@ class SeedHeuristic(Scene):
             seed = query[i:i+k]
             seed.num = num%len(colors)
             seed.i = i
-            seed.set_color(c[0])
+#            seed.set_color(c[0])
             seed.text = query.text[i:i+k]
-            seed_label = Tex("$s_{}$".format(num))
-            #seed_box = SurroundingRectangle(seed, buff=0.0, color=c[0])
+            seed_label = Tex("$s_{}$".format(num), color=c[0])
+            seed.seed_box = SurroundingRectangle(seed, buff=0.06, color=c[0])
             #seed_box.set_fill(color=c[1], opacity=1)
-            seed.seed_box = BackgroundRectangle(seed, buff=0.0, color=c[0])
+            #seed.seed_box = BackgroundRectangle(seed, buff=0.0, color=c[0], opacity=0.0)
             seed_label.next_to(seed.seed_box, UP, buff=0.1)
             seeds.add(VGroup(seed, seed_label))
-            self.play(Create(seed.seed_box), Write(seed_label))
+            self.play(
+                Create(seed.seed_box),
+                Write(seed_label),
+                seed.animate.set_color(c[0]))
             num += 1
 
         #seeds_brace_label = BraceLabel(seeds, "seeds", label_constructor=Text, brace_direction=UP, font_size=fsz)
@@ -82,7 +86,7 @@ class SeedHeuristic(Scene):
 
                 # add crumbs before this match while moving a slider backwards through the query
                 crumb = Crumb()
-                crumb.next_to(ref[j], UP, buff=SMALL_BUFF)
+                crumb.next_to(ref[j], UP, buff=1.3*SMALL_BUFF)
                 crumb.shift(delta[num])
                 ul = Underline(query[seed.i])
 
@@ -94,12 +98,11 @@ class SeedHeuristic(Scene):
                     trace = TracedPath(crumb.get_center, dissipating_time=0.5, stroke_opacity=[0, 1], stroke_color=c, stroke_width=4.0)
                     self.add(crumb, trace)
                     crumb_to = Crumb()
-                    crumb_to.next_to(ref[j], UP, buff=SMALL_BUFF)
+                    crumb_to.next_to(ref[j], UP, buff=1.3*SMALL_BUFF)
                     crumb_to.shift(delta[num])
                     self.play(
                         crumb.animate(path_arc=PI/2).move_to(crumb_to),
-                        ul.animate().move_to(Underline(query[i]))
-                    )
+                        ul.animate().move_to(Underline(query[i])))
                 self.play(
                     Uncreate(ul),
                     Uncreate(flying_box))
