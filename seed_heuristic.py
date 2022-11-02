bb5195bae74d60d0cd80eddaf074fc0f28ed1c48
Pesho Ivanov <ivanov@pesho.info>
2022-11-02 16:45:37 +0100

    works

--- seed_heuristic.py
+++ seed_heuristic.py
@@ -3,6 +3,7 @@ from manim_voiceover import VoiceoverScene
 #from manim_voiceover.services.gtts import GTTSService
 from manim_voiceover.services.azure import AzureService
 import re
+import regex
 
 #import pdb; pdb.set_trace()
 
@@ -14,6 +15,9 @@ import re
 # - another scene: Alignmnet
 
 class SeedHeuristicPrecomputation(VoiceoverScene):
+    def mywait(self):
+        self.wait(0.5)
+
     def setup_voiceover(self):
         os.environ["AZURE_SUBSCRIPTION_KEY"] = "60c24696a4da49ba94a903def1577350"
         os.environ["AZURE_SERVICE_REGION"] = "eastus"
@@ -21,16 +25,17 @@ class SeedHeuristicPrecomputation(VoiceoverScene):
         #self.set_speech_service(GTTSService())
         self.set_speech_service(
             AzureService(
-                voice="en-US-AriaNeural",
-                style="newscast-casual",  # global_speed=1.15
+                voice = "en-US-AriaNeural",
+                style ="newscast-casual",
+                global_speed = 1.15
             )
         )
 
     def construct(self):
         self.setup_voiceover()
         k = 2
-        query = Text("AACC")
-        #query = Text("AACCGGTT")
+        #query = Text("AACC")
+        query = Text("AACCGGTT")
         ref = Text("...GGAAGTCAACCGATT...") #\n\n..GATCCGGCTT..")
 
         fsz = 28
@@ -44,52 +49,74 @@ class SeedHeuristicPrecomputation(VoiceoverScene):
         colors = [blues, yellows, purples, greens]
         delta = [0.08*d for d in [UP+LEFT, UP+RIGHT, DOWN+LEFT, DOWN+RIGHT]]
 
-        # introduce reference
-        with self.voiceover(text="Assume we are a reference sequence such as a human genome.") as tracker:
-            ref.next_to(query, DOWN, buff=1.0)
-            ref_label = Text("reference", slant=ITALIC, font_size=fsz, color=grey)
-            ref_label.next_to(ref, LEFT)
-            self.play(
-                Write(ref_label),
-                Write(ref),
-                run_time=tracker.duration)
-
-        self.wait()
-
-        with self.voiceover(text="If we want to align a DNA read.") as tracker:
+        with self.voiceover(text="DNA sequencing machines produce large amount of \"reads\".") as tracker:
             query.shift(1.0*(UP+RIGHT))
-            query_label = Text("query", slant=ITALIC, font_size=fsz, color=grey)
+            query_label = Text("Query read", slant=ITALIC, font_size=fsz, color=grey)
             query_label.next_to(query, 15 * LEFT)
-            query_label.align_to(ref_label, LEFT)
             self.play(
                 Write(query_label),
                 Write(query),
                 run_time=tracker.duration)
-        self.wait()
+        self.mywait()
 
-        return
-
-        # split query into seeds
-        seeds = VGroup()
-        num = 0
-        for i in range(0,len(query),k):
-            c = colors[num%len(colors)]
-            seed = query[i:i+k]
-            seed.num = num%len(colors)
-            seed.i = i
-            seed.text = query.text[i:i+k]
-            seed_label = Tex("$s_{}$".format(num), color=c[0])
-            seed.seed_box = SurroundingRectangle(seed, buff=0.06, color=c[0])
-            seed_label.next_to(seed.seed_box, UP, buff=0.1)
-            seeds.add(VGroup(seed, seed_label))
+        # introduce reference
+        with self.voiceover(text="If we analyse a known organism, we can compare the new data to a reference genome.") as tracker:
+            ref.next_to(query, DOWN, buff=1.0)
+            ref_label = Text("Reference\ngenome", slant=ITALIC, font_size=fsz, color=grey)
+            ref_label.next_to(ref, LEFT)
+            ref_label.align_to(query_label, LEFT)
             self.play(
-                Write(seed_label),
-                seed.animate.set_color(c[0]),
-                ShowPassingFlash(
-                    seed.seed_box,
-                    run_time=2,
-                    time_width=2.0))
-            num += 1
+                Write(ref_label),
+                Write(ref),
+                run_time=tracker.duration)
+        self.mywait()
+
+        with self.voiceover(text="We will now show how to find the where each query read aligns in a reference.") as tracker:
+            # TODO: visualize the mistakes and talk about edit distance
+
+            # search for the best match (edit distance)
+            r = regex.search(r'(?b:{})'.format(query.text)+"{e}", ref.text)
+
+            # fly a box from the seed to a match
+            fly = query.copy()
+            target = ref[r.start():r.end()]
+            target_box = SurroundingRectangle(target, buff=0.06)
+            self.play(fly.animate.become(target))
+            self.play(ShowPassingFlash(
+                        target_box,
+                        run_time=2,
+                        time_width=2.0))
+        self.mywait()
+
+        with self.voiceover(text="We will use the A-star shortest path algorithm to find optimal alignments very fast.",
+                      subcaption="We will use the A* shortest path algorithm to find optimal alignments very fast.") as tracker:
+            pass
+        # TODO: add A*
+
+        with self.voiceover(text="To direct the A-star search, we will first divide the read into short seeds.") as tracker:
+            # split query into seeds
+            seeds = VGroup()
+            num = 0
+            for i in range(0,len(query),k):
+                c = colors[num%len(colors)]
+                seed = query[i:i+k]
+                seed.num = num%len(colors)
+                seed.i = i
+                seed.text = query.text[i:i+k]
+                seed_label = Tex("$s_{}$".format(num), color=c[0])
+                seed.seed_box = SurroundingRectangle(seed, buff=0.06, color=c[0])
+                seed_label.next_to(seed.seed_box, UP, buff=0.1)
+                seeds.add(VGroup(seed, seed_label))
+                self.play(
+                    Write(seed_label),
+                    seed.animate.set_color(c[0]),
+                    ShowPassingFlash(
+                        seed.seed_box,
+                        run_time=2,
+                        time_width=2.0))
+                num += 1
+        self.mywait()
+        return
 
         def CrumbFactory(num, c):
             sq_f = lambda: Square(color=c, fill_color=c, fill_opacity=1, side_length=0.08)
